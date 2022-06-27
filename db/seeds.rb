2f7d78bb78251d5af8293a878f09b996bbd02dba
TheStanish <tstan2310@gmail.com>
2022-06-27 13:42:07 -0400

    added admin functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,26 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'tyler@gmail.com', password:'password', password_confirmation: 'password')
+User.create(email: 'tyler@example.com', 
+    password:'password', 
+    password_confirmation: 'password',
+    name: 'Tyler',
+    role: User.roles[:admin])
+
+User.create(email: 'bibi@example.com', 
+    password:'password', 
+    password_confirmation: 'password',
+    name: 'Bibi')    
+
 
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here idk", user_id: User.first.id)
-end 
+    post = Post.create(title: "Title #{x}",
+                       body: "Body #{x} we got some words over here",
+                       user_id: User.second.id)
+
+5.times do |y|
+    Comment.create(body: "Comment #{y}",
+                   user_id: User.first.id,
+                   post_id: post.id)
+    end
+end
